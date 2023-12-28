15357f77cb55042026ac9992b42d22cff4fc1780
VAIDEHI KALE <143740135+vaidehi1406@users.noreply.github.com>
2023-12-29 00:00:43 +0530

    Update schoolmanagement.c
    added function and some changes

--- schoolmanagement.c
+++ schoolmanagement.c
@@ -4,12 +4,19 @@
 #include<stdlib.h>
 #include<time.h>
 
+struct student{
+    char nam[30];
+    char cl[10];
+    int rl;
+    char date[12];
+}s;
+
 int main(){
     int ch;
 
     while(1){
         system("cls");
-        printf(":--------------SCHOOL MANAGEMENT------------:");
+        printf(":--------------SCHOOL MANAGEMENT------------:\n\n");
         printf("1. Take Admission\n");
         printf("2. Student Info\n");
         printf("3. Search\n");
@@ -17,6 +24,7 @@ int main(){
         printf("5. Delete\n");
         printf("6. Sort\n");
         printf("0. Exit\n\n");
+        printf("Enter your choice: ");
         scanf("%d", &ch);
       
         switch(ch)
@@ -42,5 +50,39 @@ int main(){
                 sort();
                 break;    
         }
+        printf("/nPress any key to continue.....");
+        getch();
     }
+    return 0;
+}
+
+// Function to add a student's information
+void add(){
+   
+   char myDate[12];
+   time_t t=time(NULL);
+   struct tm tm = *localtime(&t);
+   sprintf(myDate, "%02d/%02d/%d", tm.tm_mon +1, tm.tm_year + 1900);
+    printf("%s",myDate);
+}
+
+// Function to display all student's information
+void display(){
+    int i;
+}
+
+void search(){
+
+}
+
+void modify(){
+    int i;
+}
+
+void delete(){
+    int i;
+}
+
+void sort(){
+    int i;
 }
